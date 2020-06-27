2260ef0d6d65d3c0174e70608e799a716f3baa4b
Sherexmykes <sherexmykes@gmail.com>
2020-06-26 21:34:05 -0400

    mvp

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from Bst import BSTNode
 
 start_time = time.time()
 
@@ -11,18 +12,22 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
+Bst = BSTNode("")
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+for name in names_1:
+    Bst.insert(name)
+
+for name in names_2:
+    if Bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(end_time-start_time)
+#print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+#print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
-# Python has built-in tools that allow for a very efficient approach to this problem
+# Python has built-in tools that allow for a very" efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
